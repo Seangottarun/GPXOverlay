76822aff9de2d6ca2b24ade1aa33143e8ec0d6f2
Sean Wu <sean.wu20s@outlook.com>
2019-12-04 14:24:50 -0500

    Added example code and output for overlaying elevation data on video

---
+++ examples/overlay_elevation.py
@@ -0,0 +1,14 @@
+from GPXOverlay import analysis
+from GPXOverlay import overlay
+import matplotlib.pyplot as plt
+import numpy as np
+import imgkit
+
+def main():
+    # load GPX file into an Overlay object and provide a path for output video
+    elevation_overlay = overlay.Overlay('./examples/sample-data-short.gpx', './examples/video.mp4')
+    elevation_overlay.generate_elevation_frames()
+    elevation_overlay.convert_elevation_frames_to_video()
+    elevation_overlay.overlay_elevation()
+
+main()
