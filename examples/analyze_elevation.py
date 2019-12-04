11bd3e0be7215bd74b28a8d246a6bf0651a49e6d
Sean Wu <sean.wu20s@outlook.com>
2019-12-04 08:42:13 -0500

    New example for analyzing elevation profile from GPX file

---
+++ examples/analyze_elevation.py
@@ -0,0 +1,18 @@
+from GPXOverlay import analysis
+from GPXOverlay import overlay
+import matplotlib.pyplot as plt
+import numpy as np
+import imgkit
+
+def main():
+    # load GPX file into an Analysis object and graph data
+    data = analysis.Analysis('./examples/sample-data-short.gpx')
+    ele = data.elevation_data
+    fig, ax = plt.subplots()
+    ax.plot(ele, color='#2d89ef')
+    ax.set_ylabel('Elevation [m]')
+    ax.set_xlabel('Time [s]')
+    ax.set_title("Elevation Profile")
+    fig.savefig('elevation_profile.png')
+
+main()
