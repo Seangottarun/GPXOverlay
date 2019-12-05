693441541a9690ab3ba4ef776c913fd1fb9f3383
Sean Wu <sean.wu20s@outlook.com>
2019-12-04 19:00:36 -0500

    Updated example code for overlaying speed data and added example output

--- examples/overlay_speed.py
+++ examples/overlay_speed.py
@@ -1,6 +1,7 @@
 from GPXOverlay import overlay
 
 def main():
+    # load GPX file into an Overlay object and provide a path for output video
     speed_overlay = overlay.Overlay('./examples/sample-data.gpx', './examples/video.mp4')
     speed_overlay.generate_overlay_frames()
     speed_overlay.convert_overlay_to_video()
